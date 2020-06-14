0212adaacfcec726484b8fb45e24f57faa74277f
Pavel <0013202@gmail.com>
2020-06-14 17:10:04 +0300

    Added new files Router and functions, previous commit is incorrect

---
+++ vendor/libs/functions.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+ * функция для печати в нужном формате 11
+ */
+
+function debug($arr) {
+    echo '<pre>'.print_r($arr, true).'</pre>';
+}
