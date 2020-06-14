e155f2f314bc8ceae391b5bbd2bcccab904a902b
Pavel <0013202@gmail.com>
2020-06-14 19:58:44 +0300

    Added new file 404 and new metod Dispatch

--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -12,26 +12,64 @@
  * @author 1
  */
 class Router {
-    
+    /**
+     * таблица маршрутов
+     * @var array
+     */
     protected static $routes = [];
+    /**
+    *текущий маршрут
+    * @var array 
+    */
     protected static $route = [];
     
+    /**
+     * 
+     * @param type $regexp
+     * @param type $route
+     */
     public static function add($regexp, $route = []) {
         self::$routes [$regexp] = $route;      
     }
+    /**
+     * 
+     * @return type
+     */
     public static function getRoutes() {
         return self::$routes;
     }
+    /**
+     * 
+     * @return type
+     */
     public static function getRoute() {
         return self::$route;
     }
+    /**
+     * ищет URL в таблице маршрутов
+     * @param string $url входящий URL
+     * @return boolean
+     */
     public static function matchRoute($url) {
         foreach(self::$routes as $pattern => $route){
-            if ($url == $pattern){
+            if(preg_match("#$pattern#i", $url, $matches)){
+                debug($matches);
                 self::$route = $route;
                 return true;  
             }
         }
         return false;
+    }
+    /**
+     * 
+     * @param type $url  
+     */
+    public static function dispatch($url) {
+        if(self::matchRoute($url)){
+            echo 'OK';
+        } else {
+            http_response_code('404');
+            include '404.html';
+        }
     }
 }
--- vendor/libs/functions.php
+++ vendor/libs/functions.php
@@ -5,8 +5,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-/*
- * функция для печати в нужном формате 11
+/**
+ * 
+ * функция для печати в нужном формате 
  */
 
 function debug($arr) {
