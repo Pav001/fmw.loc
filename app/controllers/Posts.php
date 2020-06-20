13fdf6d3398616a60f58b71ffe3c3deed5239557
Pavel <0013202@gmail.com>
2020-06-20 23:52:44 +0300

    Added files  Main,Posts, PostsNew in controllers dir

---
+++ app/controllers/Posts.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Posts
+ *
+ * @author 1
+ */
+class Posts {
+    //put your code here
+}
